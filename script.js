fd21671a3c99423d335e5af440ffcee194ee95f1
trishaSolanki <141935315+trishaSolanki@users.noreply.github.com>
2023-12-14 10:57:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
+
+
 function fibonacci(num) {
-// your code here
-	if(num === 0){
-		return 0;
-	}else if(num === 1){
-		return 1;
-	}else{
-		let feb[0,1];
-	
-	for(let i=2; i<=num; i++){
-		feb[i] = feb[i-1] + feb[i-2];
-}
-	return feb[i];
-}
+    if(num === 0){
+        return 0;
+    }else if(num === 1){
+        return 1;
+    }else{
+        let feb = [0,1];
+    
+        for(let i=2; i<=num; i++){
+            feb[i] = feb[i-1] + feb[i-2];
+        }
+        return feb[num];
+    }
 }
 
 module.exports = fibonacci;
