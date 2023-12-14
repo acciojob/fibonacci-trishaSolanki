e6da56fe6d166427cece5c54737f7ed9408fa338
trishaSolanki <141935315+trishaSolanki@users.noreply.github.com>
2023-12-14 14:58:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
-function fibonacci(num) {
-    if(num === 0){
-        return 0;
-    }else if(num === 1){
-        return 1;
-    }else{
-        let feb = [0,1];
+// function fibonacci(num) {
+//     if(num === 0){
+//         return 0;
+//     }else if(num === 1){
+//         return 1;
+//     }else{
+//         let feb = [0,1];
     
-        for(let i=2; i<=num; i++){
-            feb[i] = feb[i-1] + feb[i-2];
-        }
-        return feb[num];
-    }
-}
+//         for(let i=2; i<=num; i++){
+//             feb[i] = feb[i-1] + feb[i-2];
+//         }
+//         return feb[num];
+//     }
+// }
 
+// module.exports = fibonacci;
+
+
+function fibonacci(num) {
+  if (num <= 0) return 0;
+  if (num === 1) return 0;
+  if (num === 2) return 1;
+  return fibonacci(num - 1) + fibonacci(num - 2);
+}
 module.exports = fibonacci;
