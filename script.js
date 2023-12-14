90239300dcf03b265257a429c39e0d3d72c4436e
trishaSolanki <141935315+trishaSolanki@users.noreply.github.com>
2023-12-14 11:03:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-
-
 function fibonacci(num) {
     if(num === 0){
         return 0;
     }else if(num === 1){
         return 1;
     }else{
-        let feb = [0,1];
+        let feb  [0,1];
     
         for(let i=2; i<=num; i++){
             feb[i] = feb[i-1] + feb[i-2];
