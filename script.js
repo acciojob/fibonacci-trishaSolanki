34f0714362feeee6d763a995191b8aae624e2e90
trishaSolanki <141935315+trishaSolanki@users.noreply.github.com>
2023-12-30 16:20:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 function fibonacci(num) {
-    if (num <= 0) {
+    if (num === 1) {
         return 0;
-    } else if (num === 1 || num === 2 || num === 3 || num === 4 || num === 5) {
-        return num - 1;
+    } else if (num === 2) {
+        return 1;
     } else {
-        return fibonacci(num - 2) + fibonacci(num - 1);
+        return fibonacci(num - 1) + fibonacci(num - 2);
     }
 }
 
 module.exports = fibonacci;
-
