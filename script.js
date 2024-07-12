99a5cfdf432671a22de5aa03a575a10126baba99
trishaSolanki <141935315+trishaSolanki@users.noreply.github.com>
2024-07-12 13:05:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 function fibonacci(num) {
-    if (num === 1) {
-        return 0;
-    } else {
-        return fibonacci(num - 1) + fibonacci(num - 2);
+    if (num === 0) return 0;
+    if (num === 1) return 1;
+
+    let a = 0, b = 1, temp;
+    for (let i = 2; i <= num; i++) {
+        temp = a + b;
+        a = b;
+        b = temp;
     }
+    return b;
 }
 
 module.exports = fibonacci;
